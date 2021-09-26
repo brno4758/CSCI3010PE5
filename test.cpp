61dc0a3a07c3ce89343e7419dfbd1b9a513b0986
Brian <brno4758@colorado.edu>
2021-09-26 17:10:53 -0600

    modify first test case

--- test.cpp
+++ test.cpp
@@ -8,8 +8,10 @@ TEST_CASE ( "Rectangle points are constructed", "[rectangle]") {
   Point p1 = {.x = 0, .y = 0};
   Point p2 = {.x = 1, .y = 1};
   Rectangle newRectangle = Rectangle(p1,p2);
-  REQUIRE(newRectangle.get_p1() == p1);
-  REQUIRE(newRectangle.get_p2() == p2);
+  REQUIRE(newRectangle.get_p1().x == 0);
+  REQUIRE(newRectangle.get_p1().y == 0);
+  REQUIRE(newRectangle.get_p1().x == 1);
+  REQUIRE(newRectangle.get_p2().y == 1);
 }
 
 
