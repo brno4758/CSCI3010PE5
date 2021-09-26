5157285b24ae2c87d64c1618a92f4531c423ec78
Brian <brno4758@colorado.edu>
2021-09-26 17:57:26 -0600

    Added expand function test

--- test.cpp
+++ test.cpp
@@ -14,6 +14,13 @@ TEST_CASE ( "Rectangle points are constructed as p1=(0,0) and p2=(1,1)", "[recta
   REQUIRE(newRectangle.get_p2().y == 1);
 }
 
+//Invalid rectangle
+// TEST_CASE ( "Rectanlge points are constructed as p1=(1,1) and p2=(0,0), invalid rectangle","[rectangleinvalid]"){
+//   Point p1 = {.x = 1, .y = 1};
+//   Point p2 = {.x = 0, .y = 0};
+//   Rectangle newRectangle = Rectangle(p1,p2);
+// }
+
 TEST_CASE( "Rectangle width of 2", "[width]"){
   Point p1 = {.x = 0, .y = 0};
   Point p2 = {.x = 2,.y = 2};
@@ -32,6 +39,20 @@ TEST_CASE( "Rectangle of height 3 and width 2 has an area of 6", "[area]"){
   Point p2 = {.x=2,.y=3};
   Rectangle newRectangle = Rectangle(p1,p2);
   REQUIRE(newRectangle.CalculateArea() == 6);
+}
+
+TEST_CASE( "Expand rectangle with initial points p1=(0,0) and p2=(2,2) to rectangle with points p1=(-1,-1) and p2=(3,3)"){
+  Point p1 = {.x=0,.y=0};
+  Point p2 = {.x=2,.y=2};
+  Rectangle newRectangle = Rectangle(p1,p2);
+  newRectangle.Expand();
+  REQUIRE(newRectangle.get_p1().x == -1);
+  REQUIRE(newRectangle.get_p1().y == -1);
+  REQUIRE(newRectangle.get_p2().x == 2);
+  REQUIRE(newRectangle.get_p2().y == 2);
+  REQUIRE(newRectangle.GetWidth() == 3);
+  REQUIRE(newRectangle.GetHeight() == 3);
+
 }
 
 
