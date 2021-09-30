3cf1726425e40a228fc16e114b535e8204ec131c
brno4758 <brno4758@colorado.edu>
2021-09-29 22:26:51 -0600

    myRectangle finished

--- myRectangle.cpp
+++ myRectangle.cpp
@@ -3,10 +3,18 @@
 
 //BUGS IN ORIGINAL:
 //Something is wrong with the overlap funciton
-//
+//If you create a triangle w/ p1 as upper right and p2 as lower left, height and width are returned as negative
 
 
-Rectangle::Rectangle(Point p1, Point p2):p1_(p1),p2_(p2) {}
+Rectangle::Rectangle(Point p1, Point p2) {
+    if(p1.x > p2.x || p1.y > p2.y)
+    {
+        std::cout << "Invalid rectangle <Required format: p1-lower left point  p2-upper right point>\nexiting.";
+        exit(1);
+    }
+    p1_ = p1;
+    p2_ = p2;
+}
 
 
 int Rectangle::GetWidth()
@@ -42,7 +50,7 @@ void Rectangle::Shrink()
 {
     if(p1_.x==p2_.x || p1_.y==p2_.y)
     {
-        std::cout << "Cannot shrink further\n";
+        std::cout << "Cannot shrink further\n Current points are p1:(" << p1_.x << "," << p1_.y << ")  p2:(" << p2_.x << "," << p2_.y << ")\n";
         return;
     }
     p1_.x += 1;
